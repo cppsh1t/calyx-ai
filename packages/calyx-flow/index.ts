ff12da1d424189da8ddf956689e4e74ebfa03575
cppsh1t <404not@exist.com>
2026-04-13 15:10:22 +0800

    feat(calyx-flow): export Option utilities from index
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- packages/calyx-flow/index.ts
+++ packages/calyx-flow/index.ts
@@ -1,7 +1,8 @@
 // Core implementation exports
-
-
 export { buildProviderFactory } from '@/services/provider-factory.ts'
+
+// Utility exports
+export { None, Some, unwrap, unwrapOr } from '@/utils/structure.ts'
 
 // Type exports (unified export from types directory)
 export type * from '@/types/index.ts'
